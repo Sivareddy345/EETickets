ce007aaf9ac0fb87058aa9b16e280be0c420c3dd
siva_thavanam <siva_thavanam@GDCIT-DTP060>
2023-02-02 15:13:55 +0530

    Add project files.

---
+++ EETickets/Models/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "ProfilePictureURL")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name = "FullName")]
+        public string FullName { get; set; }
+        [Display(Name = "Biography")]
+        public string Bio { get; set; }
+        //realtion
+        public List<Actor_Movie> actor_Movies { get; set; }
+    }
+}
